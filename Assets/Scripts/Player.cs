a3803097fe383ee9246f4cc64a67f8e763876ade
Gal Afik <gal.p.afik@gmail.com>
2020-04-23 08:30:54 -0400

    First commit - physics object, player controller scripts added. Added tilemaps for the Ruins, Past, Walls, and Background

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace TimeRuins
+{
+	public class Player : MonoBehaviour
+	{
+		[Serializable]
+		public class ConfigurationData
+		{
+			public float MoveSpeed; // How fast the player moves
+			public float JumpForce; // How high the player can jump
+			public float Gravity; // How fast the player falls
+		}
+		public ConfigurationData Conf = new ConfigurationData();
+	}
+}
